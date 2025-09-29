24baafd9f8d3b9d0455f3ec4d509f694bd2739c5
Pablo PRUDHOMMEAU <pablo.prudhommeau@gmail.com>
2025-09-29 21:41:51 +0200

    remove broken ohlcv feature

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import {Routes} from '@angular/router';
 import {DashboardComponent} from './pages/dashboard.component';
-import {ToolsComponent} from './pages/tools.component';
 
 export const routes: Routes = [
     {path: '', component: DashboardComponent},
-    {path: 'tools', component: ToolsComponent},
     {path: 'dashboard', redirectTo: '', pathMatch: 'full'},
     {path: '**', redirectTo: ''}
 ];
